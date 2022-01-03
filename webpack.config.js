0689460b5c766cf3b6e8b208464ff5ce03462f34
Luis Fernando <fernu292@gmail.com>
2022-01-02 18:16:52 -0600

    Cambiando variables env

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT;
 
-
 module.exports = {
     name: 'express-server',
     entry: "./src/index.ts",
